e60e40d1a7663a629bbb3e36373efd51f46cacb0
yanfei <ityanfei@126.com>
2018-06-20 16:04:11 +0800

    init project

---
+++ privilege-manage-service/src/main/java/com/yanfei/scaffolding/privilege/PrivilegeManageServiceApplication.java
@@ -0,0 +1,12 @@
+package com.yanfei.scaffolding.privilege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivilegeManageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivilegeManageServiceApplication.class, args);
+	}
+}
